b7383496f5038fd1472c853fee67f71e6835d961
Victor Camargo <m4inh4@gmail.com>
2018-05-14 19:31:30 +0100

    WIP

---
+++ src/Book.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+class Book extends React.Component {
+
+  shelfDidchange(value) {
+    console.log(value);
+  }
+
+  getAuthors() {
+    const book = this.props.book
+
+    if (book.author) {
+      {book.authors.map((author, index) => {
+        return (index === book.authors.length - 1) ? author : author + ', '
+      })}
+    }
+
+    return ''
+  }
+
+  render() {
+    const { book } = this.props
+
+    return(
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+          <div className="book-shelf-changer">
+            <select value={book.shelf} onChange={(event) => this.shelfDidchange(event.target.value)}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{this.getAuthors()}</div>
+      </div>
+    )
+  }
+}
+
+export default Book
---
+++ src/SearchBooks.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
+
+class SearchBooks extends React.Component {
+
+  state = {
+    books: []
+  }
+
+  updateQuery(query) {
+    if (query === '') {
+      this.setState({ books: [] })
+
+      return
+    }
+
+    BooksAPI.search(query).then((result) => {
+      console.log(result);
+      if (result.error) {
+        this.setState({ books: [] })
+      } else {
+        this.setState({ books: result })
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to='/'>Close</Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.books.map((book) => (
+              <li key={book.id}>
+                <Book book={book} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchBooks

a59ab4a819d0a14232e17f438e21a7ea2c7217b7
Victor Camargo <m4inh4@gmail.com>
2018-05-15 11:46:15 +0100

    Prevent error when thumbnail does not exist

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ class Book extends React.Component {
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.smallThumbnail})` }}></div>
           <div className="book-shelf-changer">
             <select value={book.shelf} onChange={(event) => this.shelfDidchange(event.target.value)}>
               <option value="none" disabled>Move to...</option>
